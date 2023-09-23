3bc4b207dcd6f77cfff9165b12941b89db7b05e5
Taeyang Jin (Theo) <sun@dataportal.kr>
2023-09-24 00:47:10 +0900

    deploy to gh-pages

--- components/Image.tsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable @next/next/no-img-element */
-import { ImageProps } from 'next/image'
-
-// @ts-ignore
-const Image = ({ ...rest }: ImageProps) => <img {...rest} />
-
-export default Image
